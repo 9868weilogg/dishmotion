d9f2b7eafdd957d3e58387d35424a151a2f8f5a8
Cheah Wei Logg <william.wlcheah@gmail.com>
2019-01-08 23:41:35 +0800

    Split dishmotion from weilogg repo to its individual repo

---
+++ resources/views/dishmotion/soup.blade.php
@@ -0,0 +1,119 @@
+@extends('layouts.dishmotion-app')
+
+@section('title')
+Soup Delivery
+@endsection
+
+@section('content')
+	
+	<div class="row products">
+		<div class="col-md-6">
+			<h1 class="products">
+				淮山鲜菜汤
+			</h1>
+			<img src="/../../image/dishmotion/huai_san_xian_cai_tang.jpg">
+		</div>
+		<div class="col-md-6">
+			<h1 class="products">
+				红枣黑豆汤
+			</h1>
+			<img src="/../../image/dishmotion/heidou_paigu_tang.jpg">
+		</div>
+		<div class="col-md-6">
+			<h1 class="products">
+				木瓜花生汤
+			</h1>
+			<img src="/../../image/dishmotion/mugua_huasheng_tang.jpg">
+		</div>
+		<div class="col-md-6">
+			<h1 class="products">
+				ABC汤
+			</h1>
+			<img src="/../../image/dishmotion/ABC_tang.jpg">
+		</div>
+		<div class="col-md-6">
+			<h1 class="products">
+				参芪汤
+			</h1>
+			<img src="/../../image/dishmotion/chenqi_tang.jpg">
+		</div>
+		<div class="col-md-6">
+			<h1 class="products">
+				花生莲藕汤
+			</h1>
+			<img src="/../../image/dishmotion/peanut lotus soup.jpg">
+		</div>
+		<div class="col-md-6">
+			<h1 class="products">
+				补脑汤
+			</h1>
+			<img src="/../../image/dishmotion/brain_tonic_soup.jpg">
+		</div>
+		<div class="col-md-6">
+			<h1 class="products">
+				金瓜苹果雪耳美容汤
+			</h1>
+			<img src="/../../image/dishmotion/apple_snow_fungus_soup.jpg">
+		</div>
+		<div class="col-md-6">
+			<h1 class="products">
+				木瓜花生排骨汤 + 火腿长豆饭
+			</h1>
+			<img src="/../../image/dishmotion/papaya_peanut_soup.jpg">
+		</div>
+		<div class="col-md-6">
+			<h1 class="products">
+				灵芝清补汤 + 玉米火腿饭
+			</h1>
+			<img src="/../../image/dishmotion/lingzhi_mushroom_herbal_soup.jpg">
+		</div>
+		<div class="col-md-6">
+			<h1 class="products">
+				卤肉饭
+			</h1>
+			<img src="/../../image/dishmotion/braised_pork_rice.jpg">
+		</div>
+		<div class="col-md-6">
+			<h1 class="products">
+				蔬菜汤 + 豆角火腿饭
+			</h1>
+			<img src="/../../image/dishmotion/4vege_soup.jpg">
+		</div>
+		<div class="col-md-6">
+			<h1 class="products">
+				沙参润肺汤
+			</h1>
+			<img src="/../../image/dishmotion/shashen_runfei_tang.jpg">
+		</div>
+		<div class="col-md-6">
+			<h1 class="products">
+				咸菜豆腐汤
+			</h1>
+			<img src="/../../image/dishmotion/xiancai_tang.jpg">
+		</div>
+		<div class="col-md-6">
+			<h1 class="products">
+				苹果玉米胡萝卜汤 
+			</h1>
+			<img src="/../../image/dishmotion/corn_apple_soup.jpg">
+		</div>
+		<div class="col-md-6">
+			<h1 class="products">
+				土豆焖鸡
+			</h1>
+			<img src="/../../image/dishmotion/potato_chicken.jpg">
+		</div>
+		<div class="col-md-6">
+			<h1 class="products">
+				雪耳美容汤 + 豆角焖饭  
+			</h1>
+			<img src="/../../image/dishmotion/beauty_soup.jpg">
+		</div>
+		<div class="col-md-6">
+			<h1 class="products">
+				白萝卜排骨汤 ＋ 火腿豆角焖饭
+			</h1>
+			<img src="/../../image/dishmotion/radish_rib_soup.jpg">
+		</div>
+	</div>
+@endsection
---
+++ resources/views/layouts/dishmotion-app.blade.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<title>
+	@yield('title') | Dishmotion - soup-rice delivery service around Universiti Putra Malaysia,Serdang.
+	</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+	<!-- Popper JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+	<!-- owner's style remote&local server-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/dishmotion-style.css') }}">
+
+	<!-- footer mdbootstrap code -->
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!-- Bootstrap core CSS -->
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Material Design Bootstrap -->
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.10/css/mdb.min.css" rel="stylesheet">
+</head>
+<body>
+	
+		<div class="row header">
+			<h1 id="logo" class="header">
+				<a href="/home">Dishmotion</a> <span class="header">
+				soup-rice delivery service around Universiti Putra Malaysia,Serdang.
+			</span>
+		</div>
+			</h1>
+			
+	@yield('content')
+
+	
+	<!-- Footer -->
+	<footer class="page-footer font-small blue">
+
+	  <!-- Copyright -->
+	  <div class="footer-copyright text-center py-3">&copy; 2015 Copyright:
+	    <a href="http://weilogg.com">weilogg.com</a>
+	  </div>
+	  <!-- Copyright -->
+	  <!-- Copyright -->
+	  <div class="footer-copyright text-center py-3">
+		<a href="/admins">Admin</a>
+	  </div>
+	  <!-- Copyright -->
+
+	</footer>
+	<!-- Footer -->
+	
+</body>
+</html>
